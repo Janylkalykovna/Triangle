47513f61024b83e15a5543bd1ce68d98310ea2b6
User <janylkalykovna>
2022-05-09 21:32:39 +0600

    OOP task

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        triangle.sideA=3;
+        triangle.sideB=3;
+        triangle.sideC=5;
+        triangle.area();
+
+    }
+
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Triangle {
+    int sideA;
+    int sideB;
+    int sideC;
+
+    void area(){
+      int s = (sideA*sideB*sideC)/2;
+        System.out.println("("+sideA+"*"+sideB+"*"+sideC+")/ 2="+s);
+
+    }
+
+}
